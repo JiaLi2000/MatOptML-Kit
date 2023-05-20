6dd89695d1b2f27b8f0769352599e611f03909c7
Jia Li <jiali@stu.ecnu.edu.cn>
2023-05-20 23:39:41 +0800

    add PLU

--- src/linear_system.py
+++ src/linear_system.py
@@ -26,17 +26,18 @@ def LU(A):
     np.fill_diagonal(L, 1)
     return L, np.triu(A)
 
+
 def PLU(A):
     n = A.shape[0]
-    p = np.zeros(n-1)
+    p = np.zeros(n - 1)
     for i in range(n - 1):
-        maxp = np.argmax(np.abs(A[i:, i])) + i
+        maxp = np.argmax(np.abs(A[i:, i])) + i  # 选择最大元素作为列主元
         p[i] = maxp
-        temp = A[maxp,:].copy()
-        A[maxp, :] = A[i,:]
+        temp = A[maxp, :].copy()
+        A[maxp, :] = A[i, :]  # 同时互换了L_i
         A[i, :] = temp
-        A[i + 1:, i] = A[i + 1:, i] / A[i, i]  # 画家算法,计算L的第i列, 即高斯变换L_i非01元素取反
-        A[i + 1:, i + 1:] -= A[i + 1:, i][:, None] @ A[i, i + 1:][None, :]  # 对剩余的子式执行相应的初等行变换
+        A[i + 1:, i] = A[i + 1:, i] / A[i, i]
+        A[i + 1:, i + 1:] -= A[i + 1:, i][:, None] @ A[i, i + 1:][None, :]
     L = np.tril(A)
     np.fill_diagonal(L, 1)
 
@@ -44,34 +45,35 @@ def PLU(A):
 
 
 if __name__ == '__main__':
-        np.random.seed(10)
-        A = np.random.random((5, 5)) * 10
-        # 前代
-        L = np.tril(A)
-        np.fill_diagonal(L, 2)
-        print(L)
-        b = np.array([5.0, 3, 7, 6, 2])
-        print(forward_sub(L, b.copy()))
-        print(np.linalg.solve(L, b.copy()))
-        # 回代
-        U = np.triu(A)
-        np.fill_diagonal(U, 2)
-        print(U)
-        b = np.array([5.0, 3, 7, 6, 5])
-        print(backward_sub(U, b.copy()))
-        print(np.linalg.solve(U, b.copy()))
-        # LU
-        A = np.array([[1, 4, 7], [2, 5, 8], [3, 6, 10]])
-        print(LU(A.copy()))
-        from scipy.sparse.linalg import splu
-        slu = splu(A, diag_pivot_thresh=0)  # 等效于不执行置换的LU分解
-        print(slu.L.toarray(), slu.U.toarray())
-        # PLU
-        print('PLU')
-        import scipy.linalg as sl
-        A = np.array([[2.0, 5, 2, 7], [5, 2, 2, 8], [7, 5, 6, 6], [5, 4, 4, 8]])
-        p,L,U = PLU(A.copy())
-        print(p,'\n',L,U)
-        print('-----')
-        p,L,U = sl.lu(A.copy())
-        print(p,L,U)
+    np.random.seed(10)
+    A = np.random.random((5, 5)) * 10
+    # 前代
+    L = np.tril(A)
+    np.fill_diagonal(L, 2)
+    print(L)
+    b = np.array([5.0, 3, 7, 6, 2])
+    print(forward_sub(L, b.copy()))
+    print(np.linalg.solve(L, b.copy()))
+    # 回代
+    U = np.triu(A)
+    np.fill_diagonal(U, 2)
+    print(U)
+    b = np.array([5.0, 3, 7, 6, 5])
+    print(backward_sub(U, b.copy()))
+    print(np.linalg.solve(U, b.copy()))
+    # LU
+    A = np.array([[1, 4, 7], [2, 5, 8], [3, 6, 10]])
+    print(LU(A.copy()))
+    from scipy.sparse.linalg import splu
+    slu = splu(A, diag_pivot_thresh=0)  # 等效于不执行置换的LU分解
+    print(slu.L.toarray(), slu.U.toarray())
+    PLU
+    print('PLU')
+    import scipy.linalg as sl
+
+    A = np.array([[1, 2.0, 0], [1, 2, 1], [0, 2, 0]])
+    p, L, U = PLU(A.copy())
+    print(p, '\n', L, '\n\n', U)
+    print('-----')
+    p,L,U = sl.lu(A.copy())
+    print(p,L,U)
